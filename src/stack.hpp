9ea247f22334a64121b25d1bf40f6cdfae0738c1
esteban <estebancalvo0808@gmail.com>
2023-05-10 14:04:40 -0400

    added stack

--- src/stack.hpp
+++ src/stack.hpp
@@ -1,21 +1,20 @@
-#ifndef QUEUE
-#define QUEUE
+#ifndef STACK
+#define STACK
 
 #include <iostream>
 #include "singlyLinkedList.hpp"
 
-
 template <typename T>
-class Queue{
+class Stack{
     public:
 
         /// @brief Default Constructor
-        Queue();
+        Stack();
 
-        /// @brief Clears the queue and sets size to 0
-        ~Queue();
+        /// @brief Clears the Stack and sets size to 0
+        ~Stack();
 
-        /// @brief Gets the size of the queue
+        /// @brief Gets the size of the Stack
         /// @return Returns the size
         unsigned int size();
 
@@ -25,15 +24,12 @@ class Queue{
         /// @brief Insert element at the end
         void push(T data);
         
-        /// @brief Access the first element
-        /// @return References to last element
-        T& front();
+        /// @brief Access the top of the stack
+        /// @return References to top
+        T& top();
 
-        /// @brief Access the last element
-        /// @return Reference to last element
-        T& back();
 
-        /// @brief Checks whether queue is empty
+        /// @brief Checks whether Stack is empty
         /// @return Returns true if size = 0
         bool empty();
         
@@ -42,42 +38,40 @@ class Queue{
 };
 
 template <typename T>
-Queue<T>::Queue(){
+Stack<T>::Stack(){
     list = new SinglyLinkedList<T>();
 }
 
 template <typename T>
-Queue<T>::~Queue(){
+Stack<T>::~Stack(){
     delete list;
 }
 
 template <typename T>
-unsigned int Queue<T>::size(){
+unsigned int Stack<T>::size(){
     return list->size();
 }
 
 template <typename T>
-void Queue<T>::pop(){
-    list->removeAt(0);
+void Stack<T>::pop(){
+    if(list->size() > 0)
+        list->removeAt(list->size() - 1);
+    else
+        throw std::out_of_range("Stack is empty");
 }
 
 template <typename T>
-void Queue<T>::push(T data){
+void Stack<T>::push(T data){
     list->push_back(data);
 }
 
 template <typename T>
-T& Queue<T>::front(){
-    return (*list)[0];
+T& Stack<T>::top(){
+    return (*list)[list->size() - 1];
 }
 
 template <typename T>
-T& Queue<T>::back(){
-    return (*list)[list->size()-1];
-}
-
-template <typename T>
-bool Queue<T>::empty(){
+bool Stack<T>::empty(){
     return list->size() == 0;
 }
 
