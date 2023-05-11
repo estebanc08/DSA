f77ece4d9da6439122dd2f70b042579a8fbead85
esteban <estebancalvo0808@gmail.com>
2023-05-11 15:25:14 -0400

    added red black tree

--- src/main.cpp
+++ src/main.cpp
@@ -1,21 +1,26 @@
 #include "main.hpp"
 #include <random>
-int main(){
-    Set<int> set;
-    std::mt19937 rng(std::random_device{}());
+#include <set>
 
+int main(){
+    std::mt19937 rng(std::random_device{}());
     // Generate a random integer between 1 and 100
-    std::uniform_int_distribution<int> dist(1,100);
-    set.insert(1);
-    set.remove(1);
-    if(set.find(1) != set.end())
-        std::cout << "found\n";
-    else
-        std::cout << "Not found\n";
-    set.insert(2);
-    if(set.find(2) != set.end())
-        std::cout << "found\n";
-    else
-        std::cout << "Not found\n";
+    std::uniform_int_distribution<int> dist(0,100000);
+    std::vector<int> v;
+    RedBlackTree<int> tree;
+    for(unsigned int i = 0; i < 100000; i++){
+        tree.insert(dist(rng));
+    }
+    
+    for(unsigned int i = 0; i < 1000; i++){
+            std::uniform_int_distribution<int> offset(0,tree.size() -1);
+            auto it = tree.begin();
+            unsigned int toDel = offset(rng);
+            for(unsigned int i = 0; i < toDel; i++){
+                it++;
+            }
+            unsigned int val = (*it);
+            tree.remove(val);
+    }
     return 0;
 }
--- src/main.hpp
+++ src/main.hpp
@@ -3,3 +3,6 @@
 #include "bst.hpp"
 #include "queue.hpp"
 #include "set.hpp"
+#include "stack.hpp"
+#include "heap.hpp"
+#include "redBlackTree.hpp"
