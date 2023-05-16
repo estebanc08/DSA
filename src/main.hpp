714b29f87758928952e6b0d0d8a9b5b0be589538
esteban <estebancalvo0808@gmail.com>
2023-05-15 20:57:05 -0400

    moved all to folders

--- src/main.hpp
+++
@@ -1,9 +0,0 @@
-#include <SFML/Graphics.hpp>
-#include "singlyLinkedList.hpp"
-#include "bst.hpp"
-#include "queue.hpp"
-#include "set.hpp"
-#include "stack.hpp"
-#include "heap.hpp"
-#include "redBlackTree.hpp"
-#include "map.hpp"
